fd7a203f7ae3f8fa5218720a329583e2b28ad05a
tranthuytan <thuytan03vn@gmail.com>
2022-06-13 18:05:08 +0700

    getById Test and fail Create Test

--- ProductManagement/ProductRepository.cs
+++ ProductManagement/ProductRepository.cs
@@ -15,11 +15,16 @@ namespace ProductManagement
         {
             cs = @"Server=DESKTOP-HPMUS7S\SQLEXPRESS; Database=PRN_ProductDB; User Id=sa; Password=thuytan123";
         }
-        public int Create(Product entity)
+        public void Create(Product entity)
         {
             if (entity.Name.Trim().Equals("") || entity.Name.Length > 50)
                 throw new ArgumentException("The name must have [1,50] characters");
-
+            //if (entity.CreateDate > DateTime.UtcNow)
+            //    throw new ArgumentException("The date must not exceed today");
+            //if (entity.Price < 0)
+            //    throw new ArgumentException("The price must larger than 0");
+            //if (entity.CategoryId < 0)
+            //    throw new ArgumentException("The category ID must larger than 0");
             try
             {
                 conn = new SqlConnection(cs);
@@ -31,11 +36,10 @@ namespace ProductManagement
                 conn.Open();
                 cmd.ExecuteNonQuery();
                 conn.Close();
-                return entity.Id;
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
+                Console.WriteLine(e.StackTrace);
                 throw;
             }
         }
@@ -98,7 +102,7 @@ namespace ProductManagement
             try
             {
                 SqlConnection conn = new SqlConnection(cs);
-                SqlCommand cmd = new SqlCommand("select * from Product WHERE Id=@id AND Status=1", conn);
+                SqlCommand cmd = new SqlCommand("select Id,Name,CreateDate,Price,Status,CategoryId from Product WHERE Id=@id AND Status=1", conn);
                 cmd.Parameters.AddWithValue("@id", id);
                 conn.Open();
                 var rs = cmd.ExecuteReader();
@@ -114,7 +118,8 @@ namespace ProductManagement
                         Status = (int)rs["Status"],
                         CategoryId = (int)rs["CategoryId"]
                     };
-                    return Pd;
+                    if (Pd != null)
+                        return Pd;
                 }
                 conn.Close();
                 return null;
--- ProductManagementTest/NUnitTest.cs
+++ ProductManagementTest/NUnitTest.cs
@@ -9,20 +9,82 @@ namespace ProductManagementTest
         [SetUp]
         public void Setup()
         {
-            product = new Product();
             productRepo = new ProductRepository();
         }
-
         [Test]
-        public void Test1()
+        public void GetByIdTest()
         {
             //Assign
-
+            product.Id = 12;
+            product.Name = "red bull";
+            product.CreateDate = new DateTime(2022, 6, 13);
+            product.Price = 12000;
+            product.Status = 1;
+            product.CategoryId = 1;
             //Act
+            var dbProduct = productRepo.GetById(product.Id);
 
-            //Assert
+            //Expectation: dbProduct == product
+            Assert.AreEqual(product.Id, dbProduct.Id);
+            Assert.AreEqual(product.Name, dbProduct.Name);
+            Assert.AreEqual(product.CreateDate, dbProduct.CreateDate);
+            Assert.AreEqual(product.Price, dbProduct.Price);
+            Assert.AreEqual(product.Status, dbProduct.Status);
+            Assert.AreEqual(product.CategoryId, dbProduct.CategoryId);
+        }
+        [Test]
+        public void GetByIdWithIdOutOfRange()
+        {
+            //Assign
+            product.Id = 120;
+            product.Name = "red bull";
+            product.CreateDate = new DateTime(2022, 6, 13);
+            product.Price = 12000;
+            product.Status = 1;
+            product.CategoryId = 1;
 
-            Assert.Pass();
+            Assert.AreEqual(null,productRepo.GetById(product.Id));
+
+        }
+        [Test]
+        public void CreateProductTest()
+        {
+            //Assign
+            //database id: int identity
+            product.Id = 14;
+            product.Name = "revive lemon salt";
+            product.CreateDate = new DateTime(2022, 6, 13);
+            product.Price = 15000;
+            product.Status = 1;
+            product.CategoryId = 1;
+            //Act
+            int id = product.Id;
+            var dbProduct = productRepo.GetById(id);
+            //Expectation 1: product is not null
+            Assert.IsNotNull(dbProduct);
+            //Expectation 2: dbProduct == product
+            Assert.AreEqual(product.Id, dbProduct.Id);
+            Assert.AreEqual(product.Name, dbProduct.Name);
+            Assert.AreEqual(product.CreateDate, dbProduct.CreateDate);
+            Assert.AreEqual(product.Price, dbProduct.Price);
+            Assert.AreEqual(product.Status, dbProduct.Status);
+            Assert.AreEqual(product.CategoryId, dbProduct.CategoryId);
+        }
+        [Test]
+        public void CreateProductTest_NameException()
+        {
+            //Assign
+            //database id: int identity
+            product = new Product();
+            product.Id = 14;
+            product.Name = "revive lemon saltanksdlaskndlaksdnasldkansdlasndaksdnlasd asd as da";
+            product.CreateDate = new DateTime(2022, 6, 13);
+            product.Price = 15000;
+            product.Status = 1;
+            product.CategoryId = 1;
+
+            var ex = Assert.Throws<ArgumentException>(() => productRepo.Create(product));
+            Assert.That(ex.Message, Is.EqualTo("The name must have [1,50] characters"));
         }
     }
 }
